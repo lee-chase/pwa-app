4f88a4cc2694507ae0f3affdf137718a292e8c61
Lee Chase <lee.chase@uk.ibm.com>
2020-02-05 17:30:19 +0000

    chore: add simple server

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    Wibble wobble
+    <Articles />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Articles from "./components/Articles";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Articles
   }
 };
 </script>
